06808b8544921245d96f8e9ce66cdec56f3b3125
tobeagram <tobiaswhetton@hotmail.com>
2024-11-08 16:49:02 +0000

    Bump the version and fix naming

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'capacitorSendIntent',
+      name: 'supernotesCapacitorSendIntent',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
